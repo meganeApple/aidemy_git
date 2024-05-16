990a07d2e62a5be2728af5746d4b8cea969c74ae
Chigusa <macbookpro14@MacBook-Pro-14.local>
2024-05-17 07:17:58 +0900

    add main

--- show.py
+++ show.py
@@ -1,6 +1,16 @@
 import random
 
-with open("./hyakunin.txt", encoding="utf-8") as f:
-    wakas = [s.strip() for s in f.readlines()]
+# with open("./hyakunin.txt", encoding="utf-8") as f:
+#     wakas = [s.strip() for s in f.readlines()]
 
-print(wakas[random.randrange(len(wakas))])
+# print(wakas[random.randrange(len(wakas))])
+
+
+def main():
+
+    with open("./hyakunin.txt", encoding="utf-8") as f:
+        wakas = [s.strip() for s in f.readlines()]
+        print(wakas[random.randrange(len(wakas))])
+
+if __name__ == '__main__':
+    main()
